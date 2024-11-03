f26c8f501673cf39b3653db6736eb47f41402e8e
Makoto Kawajiri <breacs.inc@gmail.com>
2024-11-04 00:28:23 +0900

    commit_2024/11/04_00:28

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,29 @@
 // modules
 import { memo } from 'react'
+import axios from 'axios'
 
 // styles
 import tstyle from "./page.module.css";
 
 //////////////////////////////////////////////////
 
-const MemoHome = () => {
+export default memo(function Home() {
+
+  const httpRequestsGet = async () => {
+    try {
+      const response = await axios.get('https://my-json-server.typicode.com/Makoto-Kawajiri/Portfolio_0001/');
+      console.log(response);
+    } catch(error) {
+      console.log(error)
+    }
+  }
+  httpRequestsGet();
+
   return (
     <>
     <div className={`${tstyle.page}`}>
-      
+      <p>Hello, World.</p>
     </div>{/* END page */}
     </>
   );
-};
-
-export const Home = memo(MemoHome);
+});
